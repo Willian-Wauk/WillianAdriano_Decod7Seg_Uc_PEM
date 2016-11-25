29cc6bb4235295a79218c2e150477dc9b56ab32c
Willian-Wauk <williannene1@hotmail.com>
2016-11-25 18:09:58 -0200

    Decodificador adicionado

---
+++ decod7seg/Gerador-Est.sv
@@ -0,0 +1,16 @@
+module tb();
+  logic [3:0] entrada, soma;
+  logic reset;
+  logic [6:0] saida;
+  include Decod7seg tb(.reset(reset), .entrada(entrada), .saida(saida));
+  initial
+    begin
+      soma = 4'd0;
+      entrada = 4'd0;
+      repeat(100)
+        begin
+          #10 soma ++;
+          entrada = soma;
+        end
+    end
+endmodule
